b0ac10827d805db299d87042d7c2438d31651ddf
gaurav jagpal <gauravjagpal@hotmail.com>
2024-08-25 12:15:38 +0000

    add secret key

--- library_reviews/settings.py
+++ library_reviews/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eizk1)u2!nd(wfkku81p(tmt0i5@s@#exv((-36=wy2(_+$b90'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
