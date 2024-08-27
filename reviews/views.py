e292c72b594c82eeaba8edb8d6dc67366895e429
gaurav jagpal <gauravjagpal@hotmail.com>
2024-08-27 11:18:28 +0000

    add image to book_details page

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Book
 
@@ -8,3 +8,27 @@ class BookList(generic.ListView):
     queryset = Book.objects.all()
     template_name = "reviews/index.html"
     paginate_by = 6
+
+
+def book_details(request, slug):
+    """
+    Display an individual :model:`reviews.Book`.
+
+    **Context**
+
+    ``book``
+        An instance of :model:`reviews.Book`.
+
+    **Template:**
+
+    :template:`reviews/book_details.html`
+    """
+
+    queryset = Book.objects.filter(status=1)
+    book = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "reviews/book_details.html",
+        {"book": book},
+    )
