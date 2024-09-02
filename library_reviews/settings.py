33fbc9e72b2ffeb957273777cd22697e72244afd
Gaurav Jagpal <gauravjagpal@hotmail.com>
2024-09-02 10:30:16 +0000

    debug = false

--- library_reviews/settings.py
+++ library_reviews/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-gauravjagpa-libraryrevi-cyb4me1gpzz.ws-eu115.gitpod.io',
